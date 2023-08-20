ab2873da0df6a1c67de8599c2d5e590a5f3638ed
byunn90 <kayhanturk90@Live.com>
2023-08-21 05:50:19 +1000

    population caculation

--- practice-react/src/App.js
+++ practice-react/src/App.js
@@ -2,31 +2,28 @@ import React, { useState, usEffect } from "react";
 // Objective here is create a counter that decreases and increases by 1 and decrements by 1
 // Using useState helps us update the states of hooks
 function App() {
-  const [increase, setIncrease] = useState(0);
+  const [result, setResult] = useState(0);
 
-  function Increaser() {
-    if (increase >= 10) {
-      return;
-    } else {
-      setIncrease(increase + 2);
+  function populationCalculator(population, n) {
+    // return population + n * (months * decadesLeft);
+    for (let month = 1; month <= 360; month++) {
+      population += n;
     }
+    return population;
   }
-  function Decreaser() {
-    if (increase <= 0) {
-      return;
-    } else {
-      setIncrease(increase - 2);
-    }
-  }
+
+  const handleButtonClick = () => {
+    const computedResult = populationCalculator(256, 2);
+    setResult(computedResult);
+  };
+
+  console.log(populationCalculator(256, 2));
 
   return (
     <div className="App">
       <p>hi</p>
-      <button onClick={Increaser} className="">
-        Increase
-      </button>
-      <span>{increase}</span>
-      <button onClick={Decreaser}>Decreaser</button>
+      <button onClick={handleButtonClick}>Calculate Population</button>
+      <span>{result}</span>
     </div>
   );
 }
