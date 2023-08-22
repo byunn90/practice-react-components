6d86db037dadcece72f0d52fa44a2beb8c7700fa
byunn90 <kayhanturk90@Live.com>
2023-08-22 12:30:18 +1000

    algro

--- practice-react/src/App.js
+++ practice-react/src/App.js
@@ -1,29 +1,24 @@
-import React, { useState, usEffect } from "react";
-// Objective here is create a counter that decreases and increases by 1 and decrements by 1
-// Using useState helps us update the states of hooks
+import React, { useState } from "react";
+
 function App() {
-  const [result, setResult] = useState(0);
+  const [numbers, setNumbers] = useState([]);
 
-  function populationCalculator(population, n) {
-    // return population + n * (months * decadesLeft);
-    for (let month = 1; month <= 360; month++) {
-      population += n;
+  function handleButtonClick() {
+    const newNumbers = [];
+    for (let i = 1; i <= 6; i++) {
+      newNumbers.push[i];
     }
-    return population;
+    setNumbers(newNumbers);
   }
-
-  const handleButtonClick = () => {
-    const computedResult = populationCalculator(256, 2);
-    setResult(computedResult);
-  };
-
-  console.log(populationCalculator(256, 2));
 
   return (
     <div className="App">
-      <p>hi</p>
-      <button onClick={handleButtonClick}>Calculate Population</button>
-      <span>{result}</span>
+      <button onClick={handleButtonClick}>Display Numbers</button>
+      <div>
+        {numbers.map((number) => (
+          <span key={number}>{number} </span>
+        ))}
+      </div>
     </div>
   );
 }
